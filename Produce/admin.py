0ce363775ef7018de41970c2db536776109f4605
YTeyfik <teyo_2086@hotmail.com>
2020-08-26 12:46:05 +0300

    Creating a user in the Admin Panel, establishing a relationship between the category and product and editing in the setting section for image uploading

--- Produce/admin.py
+++ Produce/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from Produce.models import Category, Produce
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display =['title','status']
+    list_filter = ['status']
+
+
+class ProduceAdmin(admin.ModelAdmin):
+    list_display = ['title','category','cost','Quantity']
+
+admin.site.register(Category,CategoryAdmin)
+admin.site.register(Produce,ProduceAdmin)
--- Produce/models.py
+++ Produce/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here.
 
+
+
 class Category(models.Model):
     STATUS = (
         ('True', 'Evet'),
@@ -19,3 +21,23 @@ class Category(models.Model):
 
     def __str__(self):
         return self.title
+
+class Produce(models.Model):
+        STATUS = (
+            ('True', 'Evet'),
+            ('False', 'Hayır'),
+        )
+        category = models.ForeignKey(Category, on_delete=models.CASCADE)  # category tablosu ile ilişki
+        title = models.CharField(blank=True, max_length=100)
+        keywords = models.CharField(blank=True, max_length=255)
+        description = models.CharField(blank=True, max_length=255)
+        image = models.ImageField(blank=True, upload_to='images/')
+        cost=models.FloatField()
+        Quantity=models.IntegerField()
+        detail = models.TextField()
+        status = models.CharField(max_length=10, choices=STATUS)
+        created_at = models.DateTimeField(auto_now_add=True)
+        updated_at = models.DateTimeField(auto_now=True)
+
+        def __str__(self):
+            return self.title
