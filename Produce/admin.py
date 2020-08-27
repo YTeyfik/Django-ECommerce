d61d9b832a4bb27335330ca98e4cfab0f144221a
YTeyfik <teyo_2086@hotmail.com>
2020-08-27 14:14:22 +0300

    Creating an image gallery for the produce in the admin panel

--- Produce/admin.py
+++ Produce/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
-from Produce.models import Category, Produce
+from Produce.models import Category, Produce, Images
 
+
+class ProduceImageInline(admin.TabularInline): #adding image product inline
+    model = Images
+    extra = 5
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display =['title','status']
@@ -11,6 +15,13 @@ class CategoryAdmin(admin.ModelAdmin):
 
 class ProduceAdmin(admin.ModelAdmin):
     list_display = ['title','category','cost','Quantity']
+    list_filter =['status','category']
+    inlines = [ProduceImageInline]
+
+
+class ImagesAdmin(admin.ModelAdmin):
+    list_display = ['title','produce','image']
 
 admin.site.register(Category,CategoryAdmin)
 admin.site.register(Produce,ProduceAdmin)
+admin.site.register(Images,ImagesAdmin)
