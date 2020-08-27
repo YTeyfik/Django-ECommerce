b22e8d82b03aeee228f1d6cfffe122ae0a8fc3a9
YTeyfik <teyo_2086@hotmail.com>
2020-08-27 16:52:27 +0300

    Adding settings to admin panel and adding favicon

--- home/models.py
+++ home/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+class Setting(models.Model):
+    STATUS = (
+        ('True', 'Evet'),
+        ('False', 'Hayır'),
+    )
+    title = models.CharField(blank=True, max_length=100)
+    keywords = models.CharField(blank=True, max_length=255)
+    description = models.CharField(blank=True, max_length=255)
+    company = models.CharField(blank=True, max_length=100)
+    address = models.CharField(blank=True, max_length=250)
+    phone = models.CharField(blank=True, max_length=100)
+    fax = models.CharField(blank=True, max_length=100)
+    email = models.CharField(blank=True, max_length=100)
+    smtpserver = models.CharField(blank=True, max_length=100)
+    smtpemail = models.CharField(blank=True, max_length=100)
+    smtppassword = models.CharField(blank=True, max_length=100)
+    smtpport = models.CharField(blank=True, max_length=100)
+    icon = models.ImageField(blank=True, upload_to='images/')
+    facebook = models.CharField(blank=True, max_length=100)
+    instagram = models.CharField(blank=True, max_length=100)
+    twitter = models.CharField(blank=True, max_length=100)
+    aboutus = models.TextField(blank=True, max_length=100)
+    contact = models.TextField(blank=True, max_length=100)
+    status = models.CharField(max_length=10, choices=STATUS)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
