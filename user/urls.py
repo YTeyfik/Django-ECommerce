db3fabab5fce6a60807250e2cd580c7912318ed1
YTeyfik <teyo_2086@hotmail.com>
2020-09-11 15:19:40 +0300

    Üye yorumları listeleme, silme Ve Proje bitti

--- user/urls.py
+++ user/urls.py
@@ -7,6 +7,8 @@ urlpatterns=[
     path('update/',views.user_update,name='user_update'),
     path('password/',views.change_password,name='change_password'),
     path('orders/',views.orders,name='orders'),
+    path('comments/',views.comments,name='comments'),
+    path('deletecomment/<int:id>',views.deletecomment,name='deletecomment'),
     path('orderdetail/<int:id>',views.orderdetail,name='orderdetail')
     #path('addcomment/<int:id>',views.addcomment,name='addcomment')
 ]
